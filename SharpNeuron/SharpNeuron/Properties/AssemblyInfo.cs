2587634b978bfcb72bcc71d6a22ffeb56390d9c3
0xFireball <0xFireball@outlook.com>
2016-08-06 11:39:50 +0530

    add source

---
+++ SharpNeuron/SharpNeuron/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("SharpNeuron")]
+[assembly: AssemblyDescription ("SharpNeuron is a Neural Network library for .NET")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("IridiumIon Software")]
+[assembly: AssemblyProduct ("SharpNeuron")]
+[assembly: AssemblyCopyright ("Copyright 2016 IridiumIon Software")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("2ee9d7cf-71a8-4347-bebd-b13fd339bda8")]
+[assembly: AssemblyVersion ("3.0.0.0")]
+[assembly: AssemblyFileVersion ("3.0.0.0")]
---
+++ SharpNeuron/SharpNeuron/TrainingMethod.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace NeuronDotNet.Core
+{
+    /// <summary>
+    /// Method of training used to train the network
+    /// </summary>
+    public enum TrainingMethod
+    {
+        /// <summary>
+        /// Supervised training method.
+        /// </summary>
+        Supervised = 0,
+
+        /// <summary>
+        /// Unsupervised training method.
+        /// </summary>
+        Unsupervised = 1
+    }
+}
