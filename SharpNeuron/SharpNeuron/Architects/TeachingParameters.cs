f2bd6e68d0836fa7082ae5617f5d82d299e3e762
0xFireball <0xFireball@outlook.com>
2016-11-11 10:31:05 -0800

    re-add source

---
+++ SharpNeuron/SharpNeuron/Architects/TeachingParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpNeuron.Architects
+{
+    public class TeachingParameters
+    {
+        /// <summary>
+        /// The maximum number of iterations to train the network for
+        /// </summary>
+        public int Iterations { get; set; } = 20000;
+
+        /// <summary>
+        /// The threshold of error at which to stop training the network. If the error of the network is below this threshold, teaching stops, even if not all of the iterations are finished. Set this to 0 to have the teacher always train for all the iterations
+        /// </summary>
+        public double ErrorThreshold { get; set; } = 0.005;
+
+        /// <summary>
+        /// The rate at which the network should learn
+        /// </summary>
+        public double LearningRate { get; set; } = 0.3;
+    }
+}
---
+++ SharpNeuron/SharpNeuron/Backpropagation/BackpropagationConnector.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SharpNeuron.Backpropagation
+{
+    /// <summary>
+    /// A Backpropagation Connector is an <see cref="IConnector"/> which consists of a collection of
+    /// backpropagation synapses connecting two activation layers.
+    /// </summary>
+    [Serializable]
+    public class BackpropagationConnector
+        : Connector<ActivationLayer, ActivationLayer, BackpropagationSynapse>
+    {
+        internal double momentum = 0.07d;
+
+        /// <summary>
+        /// Gets or sets the momentum (the tendency of synapses to retain their previous deltas)
+        /// </summary>
+        /// <value>
+        /// The tendency of synapses to retain their previous weight change.
+        /// </value>
+        public double Momentum
+        {
+            get { return momentum; }
+            set { momentum = value; }
+        }
+
+        /// <summary>
+        /// Creates a new complete backpropagation connector between given layers.
+        /// </summary>
+        /// <param name="sourceLayer">
+        /// The source layer
+        /// </param>
+        /// <param name="targetLayer">
+        /// The target layer
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// If <c>sourceLayer</c> or <c>targetLayer</c> is <c>null</c>
+        /// </exception>
+        public BackpropagationConnector(ActivationLayer sourceLayer, ActivationLayer targetLayer)
+            : this(sourceLayer, targetLayer, ConnectionMode.AllToAll)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Backpropagation connector between the given layers using the specified
+        /// connection mode.
+        /// </summary>
+        /// <param name="sourceLayer">
+        /// The source layer
+        /// </param>
+        /// <param name="targetLayer">
+        /// The target layer
+        /// </param>
+        /// <param name="connectionMode">
+        /// Connection mode to use
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// If <c>sourceLayer</c> or <c>targetLayer</c> is <c>null</c>
+        /// </exception>
+        public BackpropagationConnector(ActivationLayer sourceLayer, ActivationLayer targetLayer, ConnectionMode connectionMode)
+            : base(sourceLayer, targetLayer, connectionMode)
+        {
+            ConstructSynapses();
+        }
+
+        /// <summary>
+        /// Deserialization Constructor
+        /// </summary>
+        /// <param name="info">
+        /// Serialization information to deserialize and obtain the data
+        /// </param>
+        /// <param name="context">
+        /// Serialization context to use
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// If <c>info</c> is <c>null</c>
+        /// </exception>
+        public BackpropagationConnector(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            ConstructSynapses();
+
+            momentum = info.GetDouble("momentum");
+            double[] weights = (double[])info.GetValue("weights", typeof(double[]));
+
+            for (int i = 0; i < synapses.Length; i++)
+            {
+                synapses[i].Weight = weights[i];
+            }
+        }
+
+        /// <summary>
+        /// Populates the serialization info with the data needed to serialize the connector
+        /// </summary>
+        /// <param name="info">
+        /// The serialization info to populate the data with
+        /// </param>
+        /// <param name="context">
+        /// The serialization context to use
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// If <c>info</c> is <c>null</c>
+        /// </exception>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("momentum", momentum);
+
+            double[] weights = new double[synapses.Length];
+            for (int i = 0; i < synapses.Length; i++)
+            {
+                weights[i] = synapses[i].Weight;
+            }
+
+            info.AddValue("weights", weights, typeof(double[]));
+        }
+
+        /// <summary>
+        /// Initializes all synapses in the connector and makes them ready to undergo training
+        /// freshly. (Adjusts the weights of synapses using the initializer)
+        /// </summary>
+        public override void Initialize()
+        {
+            if (initializer != null)
+            {
+                initializer.Initialize(this);
+            }
+        }
+
+        /// <summary>
+        /// Private helper to construct synapses between layers
+        /// </summary>
+        private void ConstructSynapses()
+        {
+            int i = 0;
+            if (connectionMode == ConnectionMode.AllToAll)
+            {
+                foreach (ActivationNeuron targetNeuron in targetLayer.Neurons)
+                {
+                    foreach (ActivationNeuron sourceNeuron in sourceLayer.Neurons)
+                    {
+                        synapses[i++] = new BackpropagationSynapse(sourceNeuron, targetNeuron, this);
+                    }
+                }
+            }
+            else if (connectionMode == ConnectionMode.OneToOne)
+            {
+                IEnumerator<ActivationNeuron> sourceEnumerator = sourceLayer.Neurons.GetEnumerator();
+                IEnumerator<ActivationNeuron> targetEnumerator = targetLayer.Neurons.GetEnumerator();
+                while (sourceEnumerator.MoveNext() && targetEnumerator.MoveNext())
+                {
+                    synapses[i++] = new BackpropagationSynapse(
+                        sourceEnumerator.Current, targetEnumerator.Current, this);
+                }
+            }
+
+            else if (connectionMode == ConnectionMode.AllToElse)
+            {
+                foreach (ActivationNeuron targetNeuron in targetLayer.Neurons)
+                {
+                    foreach (ActivationNeuron sourceNeuron in sourceLayer.Neurons)
+                    {
+                        if (sourceNeuron == targetNeuron)
+                        {
+                            continue; //Skip connecting to self
+                        }
+                        synapses[i++] = new BackpropagationSynapse(sourceNeuron, targetNeuron, this);
+                    }
+                }
+            }
+        }
+
+        public static BackpropagationConnector Connect(ActivationLayer sourceLayer, ActivationLayer targetLayer)
+        {
+            return new BackpropagationConnector(sourceLayer, targetLayer);
+        }
+    }
+}
