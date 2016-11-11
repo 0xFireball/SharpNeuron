d7541585932f4bbf2a73a363e1abd2fb7b9a49d6
0xFireball <0xFireball@outlook.com>
2016-11-11 11:11:57 -0800

    some code cleanup

--- SharpNeuron/SharpNeuron.Demo/Program.cs
+++ SharpNeuron/SharpNeuron.Demo/Program.cs
@@ -16,11 +16,11 @@ namespace SharpNeuron.Demo
             var perceptron = new Perceptron(inputs, 3, outputs);
             var teacher = new PerceptronTeacher(perceptron);
 
-            TrainingSet xorTrainingSet = new TrainingSet(2, 1);
-            xorTrainingSet.Add(new TrainingSample(new double[2] { 0d, 0d }, new double[1] { 0d }));
-            xorTrainingSet.Add(new TrainingSample(new double[2] { 0d, 1d }, new double[1] { 1d }));
-            xorTrainingSet.Add(new TrainingSample(new double[2] { 1d, 0d }, new double[1] { 1d }));
-            xorTrainingSet.Add(new TrainingSample(new double[2] { 1d, 1d }, new double[1] { 0d }));
+            var xorTrainingSet = new TrainingSet(2, 1);
+            xorTrainingSet.Add(new TrainingSample(new double[] { 0d, 0d }, new double[] { 0d }));
+            xorTrainingSet.Add(new TrainingSample(new double[] { 0d, 1d }, new double[] { 1d }));
+            xorTrainingSet.Add(new TrainingSample(new double[] { 1d, 0d }, new double[] { 1d }));
+            xorTrainingSet.Add(new TrainingSample(new double[] { 1d, 1d }, new double[] { 0d }));
 
             //Do 20K iterations unconditionalls
             teacher.Train(xorTrainingSet, new TeachingParameters { Iterations = 20000, ErrorThreshold = 0 });
